65cadf69f11a1eaf078516336bde52890ade0aed
wedusganteng <63887769+wedusganteng@users.noreply.github.com>
2020-04-20 13:53:50 +0700

    Add files via upload

---
+++ anyar.py
@@ -0,0 +1,34 @@
+import time
+#variabel 
+text  ='Selamat datang di Script Mr.W3du5'
+
+print (text)
+
+#input output
+nama = input ('masukkan nama anda : ')
+
+print ('selamat datang' ,nama)
+
+#percabangan
+print('\t[MENU]')
+menu ='''
+1. spam wa 
+2. spam sms 
+3. spam call
+'''
+print(menu)
+
+
+text1 = input ('Masukkan pilihan anda : ')
+
+if text1 == '1' :
+	print ('I love You')
+	
+	
+if text1 == '2' :
+	print ('I LOVE YOU)
+	
+	
+if text1 == '3' : 
+	print ('I LOVE YOU:v')
+	
