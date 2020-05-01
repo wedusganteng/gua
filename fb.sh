3c6b6ac929b66ea5a1f91138465870c82a499a06
wedusganteng <63887769+wedusganteng@users.noreply.github.com>
2020-05-01 08:51:47 +0700

    Add files via upload

---
+++ fb.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#version 1.0
+
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+sleep 1
+
+cowsay -f eyes "Mr. Wedus" | lolcat
+figlet -f slant "Mr. Wedus" | lolcat
+echo
+echo
+echo "  <=====================[]====================>" | lolcat
+echo "  <=====[       Tools By Mr.Wedus        ]=====>" | lolcat
+echo "  <=====[  Concact Me : +62895355578413  ]=====>" | lolcat
+echo "  <=====================[]====================>" | lolcat
+echo
+echo
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+
+echo "[===============================================[>"
+echo $green "1.> Dark fb v1"
+echo "[===============================================]>"
+echo $blue "2.> Dark fb v2"
+echo "[===============================================[>"
+echo "\033[32;1m"
+read -p "root@T00Ls-Mr.W3du5~# " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet MR. W3du5 |lolcat
+sleep 1
+echo $green" I LOVE YOU "
+sleep 1
+echo $green" sorry sudah kena tipu"
+sleep 1
+echo  $green" Jangan marah ya sayang :v"
+sleep 1
+echo $red" Chat Wa : 087837482552"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet MR. W3du5 |lolcat
+sleep 1
+echo  $green" I LOVE YOU "
+sleep 1
+echo $green" sorry sudah kena tipu"
+sleep 1
+echo $green" Jangan marah ya sayang :v"
+sleep 1
+echo $red" Chat Wa : 087837482552"
+fi
